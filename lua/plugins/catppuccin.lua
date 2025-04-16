74d03b2d0fb11ba9942633949d63ee7dd3a3e41e
Eric Renfro <psi-jack@linux-help.org>
2025-04-16 17:46:39 -0400

    Initial changes from local install

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,23 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize catppuccin colorscheme
+
+return {
+    { -- further customize the options sey by the community
+        "catppuccin",
+        opts = {
+            integrations = {
+                sandwich = false,
+                noice = true,
+                mini = true,
+                leap = true,
+                markdown = true,
+                neotest = true,
+                cmp = true,
+                overseer = true,
+                lsp_trouble = true,
+                rainbow_delimiters = true,
+            },
+        },
+    },
+}
